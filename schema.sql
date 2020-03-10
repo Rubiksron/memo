da8c190a48e77cc25eb2441222c289321a46361d
Rubiksron <rubiksron@gmail.com>
2020-03-10 15:41:40 -0700

    installed ejs and got 1 view to show on route after client.query runs. Also I am connected to the db.

--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@ DROP TABLE memos;
 CREATE TABLE IF NOT EXISTS memos (
   id SERIAL PRIMARY KEY,
   memo VARCHAR(255)
-)
+);
 
-INSERT INTO memos (memo) VALUES ('test memo 1')
-INSERT INTO memos (memo) VALUES ('test memo 2')
-INSERT INTO memos (memo) VALUES ('test memo 3')
+INSERT INTO memos (memo) VALUES ('test memo 1');
+INSERT INTO memos (memo) VALUES ('test memo 2');
+INSERT INTO memos (memo) VALUES ('test memo 3');
