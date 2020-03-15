34a124bb16b3190ba2f216e9df7f9616f6ed1bf0
Rubiksron <rubiksron@gmail.com>
2020-03-15 16:31:42 -0700

    storing info into local storage and also retrieving it.  next to pass some token to confirm the user has signed in.

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ app.post('/createMemo', createMemo);
 app.delete('/delete/:id', deleteMemo);
 app.get('*', (request, response) => response.status(404).send('This Route Does Not Exist'));
 
+
 client.connect()
   .then(() => {
     app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
