b6bdb9451b9e3fd689f81c8a40e5a2250898e35d
Rubiksron <rubiksron@gmail.com>
2020-03-14 18:33:59 -0700

    set up a signup and login page and form. set up a user schema and table so that i can add new users. functionality set up to check and see if the user already exists and if the password is correct.

--- server.js
+++ server.js
@@ -31,9 +31,13 @@ const getGroceries = require('./lib/getGroceries');
 const getHardware = require('./lib/getHardware');
 const getDrugstore = require('./lib/getDrugstore');
 const deleteMemo = require('./lib/deleteMemo');
+const login = require('./lib/login');
 
 //Routes
 app.get('/', login);
+app.get('/createAccount', createAccount);
+app.post('/createUser', createUser)
+app.post('/checkPassword', checkPassword);
 app.get('/getMemos', getMemos);
 app.get('/groceries', getGroceries);
 app.get('/drugstore', getDrugstore);
@@ -42,11 +46,47 @@ app.post('/createMemo', createMemo);
 app.delete('/delete/:id', deleteMemo);
 app.get('*', (request, response) => response.status(404).send('This Route Does Not Exist'));
 
-function login(request, response) {
-  const userName = request.body.user;
-  console.log('login here!!!!!!');
-  response.render('./pages/login', { userName: userName});
+function createUser(request, response) {
+  const user = request.body.user.toLowerCase();
+  const password = request.body.password.toLowerCase();
+  const VALUES = [user, password];
+  const SQL = 'INSERT INTO users(user_name, user_password) VALUES($1, $2) RETURNING user_name, user_password;';
+  console.log('user: ', user, 'password: ', password);
+  client.query(SQL, VALUES)
+    .then(() => response.redirect('/'))
+    .catch(err => console.log('ya done goofed: ',err));
 }
+
+function createAccount(request, response) {
+  response.render('./pages/createAccount');
+}
+
+function checkPassword(request, response) {
+  const user = request.body.user.toLowerCase();
+  const password = request.body.password.toLowerCase();
+  const VALUES = [user, password];
+  // const SQLpost = 'INSERT INTO users(user_name, user_password) VALUES($1, $2) RETURNING user_name, user_password;';
+  const SQL  = 'SELECT * FROM users';
+
+  client.query(SQL)
+    .then((results) => {
+      // console.log('checking password');
+      // console.log('results.rows: ', results.rows);
+      results.rows.forEach(element => {
+        if(element.user_name === user && element.user_password === password) {
+          console.log('Identity Confirmed!');
+          // results.redirect('/getMemos');
+        } else {
+          return console.log('Unknown User!');
+          // response.redirect('/');
+        }
+
+      });
+    })
+    .then(() => response.redirect('/getMemos'))
+    .catch(err => console.log('ya done goofed: ',err));
+}
+
 
 client.connect()
   .then(() => {
