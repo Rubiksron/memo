9e82d05b639592137143eee52555dd5d19754a50
Rubiksron <rubiksron@gmail.com>
2020-03-26 15:48:12 -0700

    the database is setting the id of the user to the id of the memo! now i can track all of the memos for each of the users.

--- data/schemaMemo.sql
+++ data/schemaMemo.sql
@@ -1,14 +1,14 @@
 DROP TABLE memos;
 
 CREATE TABLE IF NOT EXISTS memos (
-  id SERIAL PRIMARY KEY,
+  id VARCHAR(255),
   memo VARCHAR(255),
   chore_type VARCHAR(255)
 );
 
-INSERT INTO memos (memo, chore_type) VALUES ('food memo 1', 'groceries');
-INSERT INTO memos (memo, chore_type) VALUES ('food memo 2', 'groceries');
-INSERT INTO memos (memo, chore_type) VALUES ('cleaning supplies memo 1', 'hardware');
-INSERT INTO memos (memo, chore_type) VALUES ('cleaning supplies memo 2', 'hardware');
-INSERT INTO memos (memo, chore_type) VALUES ('medicine 1', 'drugstore');
-INSERT INTO memos (memo, chore_type) VALUES ('medicine 2', 'drugstore');
+INSERT INTO memos (id, memo, chore_type) VALUES ('1', 'food memo 1', 'groceries');
+INSERT INTO memos (id, memo, chore_type) VALUES ('2', 'food memo 2', 'groceries');
+INSERT INTO memos (id, memo, chore_type) VALUES ('3', 'cleaning supplies memo 1', 'hardware');
+INSERT INTO memos (id, memo, chore_type) VALUES ('4', 'cleaning supplies memo 2', 'hardware');
+INSERT INTO memos (id, memo, chore_type) VALUES ('5', 'medicine 1', 'drugstore');
+INSERT INTO memos (id, memo, chore_type) VALUES ('6', 'medicine 2', 'drugstore');
