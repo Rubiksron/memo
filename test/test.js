64ff16fc51f63c714f66707232fdcc17dda3c293
Rubiksron <rubiksron@gmail.com>
2020-04-22 13:24:29 -0700

    removed one layer of describe statements from the tests.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,10 @@
-var assert = require('assert');
+'use strict';
+
+const app = require('../server');
+const supertest = require('supertest');
+const request = supertest(app);
+const superagent = require('superagent');
+
 var createUser = require('../lib/createUser');
 var createMemo = require('../lib/createMemo');
 var Client = require('../lib/Client');
@@ -14,125 +20,100 @@ var getHardware = require('../lib/getHardware');
 var getMemos = require('../lib/getMemos');
 var login = require('../lib/login');
 var logout = require('../lib/logout');
+var aboutMe = require('../lib/aboutMe');
 
-describe('logout', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof logout).toBe('function');
-    });
+describe('should be a function', function() {
+  it('aboutMe should return true when checked with typeof function', function() {
+    expect(typeof aboutMe).toBe('function');
   });
 });
 
-describe('login', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof login).toBe('function');
-    });
+describe('should be a function', function() {
+  it('logout should return true when checked with typeof function', function() {
+    expect(typeof logout).toBe('function');
   });
 });
 
-describe('getMemos', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof getMemos).toBe('function');
-    });
+describe('should be a function', function() {
+  it('login should return true when checked with typeof function', function() {
+    expect(typeof login).toBe('function');
   });
 });
 
-describe('getHardware', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof getHardware).toBe('function');
-    });
+describe('should be a function', function() {
+  it('getMemos should return true when checked with typeof function', function() {
+    expect(typeof getMemos).toBe('function');
   });
 });
 
-describe('getGroceries', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof getGroceries).toBe('function');
-    });
+describe('should be a function', function() {
+  it('getHardware should return true when checked with typeof function', function() {
+    expect(typeof getHardware).toBe('function');
   });
 });
 
-describe('getDrugstore', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof getDrugstore).toBe('function');
-    });
+describe('should be a function', function() {
+  it('getGroceries should return true when checked with typeof function', function() {
+    expect(typeof getGroceries).toBe('function');
   });
 });
 
-describe('dropDownPage', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof dropDownPage).toBe('function');
-    });
+describe('should be a function', function() {
+  it('getDrugstore should return true when checked with typeof function', function() {
+    expect(typeof getDrugstore).toBe('function');
   });
 });
 
-describe('deleteMemo', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof deleteMemo).toBe('function');
-    });
+describe('should be a function', function() {
+  it('dropDownPage should return true when checked with typeof function', function() {
+    expect(typeof dropDownPage).toBe('function');
   });
 });
 
-describe('deleteAccount', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof deleteAccount).toBe('function');
-    });
+describe('should be a function', function() {
+  it('deleteMemo should return true when checked with typeof function', function() {
+    expect(typeof deleteMemo).toBe('function');
   });
 });
 
-describe('createAccount', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof createAccount).toBe('function');
-    });
+describe('should be a function', function() {
+  it('deleteAccount should return true when checked with typeof function', function() {
+    expect(typeof deleteAccount).toBe('function');
   });
 });
 
-describe('checkPassword', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof checkPassword).toBe('function');
-    });
+describe('should be a function', function() {
+  it('createAccount should return true when checked with typeof function', function() {
+    expect(typeof createAccount).toBe('function');
   });
 });
 
-describe('beforeGetMemos', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof beforeGetMemos).toBe('function');
-    });
+describe('should be a function', function() {
+  it('checkPassword should return true when checked with typeof function', function() {
+    expect(typeof checkPassword).toBe('function');
   });
 });
 
-describe('Client.js', function() {
-  describe('should be an object', function() {
-    it('should create a new client through pg.Client() that is an object', function() {
-      expect(typeof Client).toBe('object');
-    });
+describe('should be a function', function() {
+  it('beforeGetMemos should return true when checked with typeof function', function() {
+    expect(typeof beforeGetMemos).toBe('function');
   });
 });
 
-describe('createMemo.js', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof createMemo).toBe('function');
-
-    });
+describe('should be an object', function() {
+  it('Client should create a new client through pg.Client() that is an object', function() {
+    expect(typeof Client).toBe('object');
   });
 });
 
-describe('createUser.js', function() {
-  describe('should be a function', function() {
-    it('should return true when checked with typeof function', function() {
-      expect(typeof createUser).toBe('function');
-    });
+describe('should be a function', function() {
+  it('createMemo should return true when checked with typeof function', function() {
+    expect(typeof createMemo).toBe('function');
   });
 });
 
+describe('should be a function', function() {
+  it('createUser should return true when checked with typeof function', function() {
+    expect(typeof createUser).toBe('function');
+  });
+});
